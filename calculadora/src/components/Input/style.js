b48b3d90016f2de93200a7293049096cbb23a12a
JoaoMoraisxD <joaomoraisxd@gmailcom>
2023-06-19 13:04:28 -0300

    FIT: Resolvendo bug do projeto nao subir para o repositorio remoto

---
+++ calculadora/src/components/Input/style.js
@@ -0,0 +1,28 @@
+import styled from 'styled-components'
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 150px;
+    background-color: #AAAA;
+    
+    display: flex;
+    align-items: center;
+    justify-content: flex-end;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 100%;
+        height: 150px;
+        background-color: #AAAA;
+        border: 0;
+        align: right;
+        padding: 0 10px;
+        font-size: 55px;
+        font-weight: 550;
+        font-family: 'Roboto';
+        color: #FFFFFF
+    }
+
+`
